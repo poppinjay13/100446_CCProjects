55f9403def26087e4e3417b62469e6f29def83f5
Ian Odundo <thisian13@gmail.com>
2020-11-10 21:14:13 +0300

    added integers for more complete analysis

--- lab1/analyze.cpp
+++ lab1/analyze.cpp
@@ -6,11 +6,17 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "Strathmore", "World", "from", "Ian Odundo", "at", "100446"};
+    int i = 0;
+    vector<string> msg{"Hello", "Strathmore", "World", "from", "Ian", "Odundo", "at", "100446"};
 
-    for (const string& word : msg)
+    for (const string &word : msg)
     {
+        i++;
         cout << word << " ";
     }
+
+    cout << "\n"
+         << i << " Words Displayed";
+
     cout << endl;
 }
